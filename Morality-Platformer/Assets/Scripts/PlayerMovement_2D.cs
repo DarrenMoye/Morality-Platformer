9822268f98309b4cdcd456e0fc50aecef3711b0e
DarrenMoye <darrenmoye02@gmail.com>
2022-12-03 16:59:02 +0000

    Created movement for 2D section

---
+++ Morality-Platformer/Assets/Scripts/PlayerMovement_2D.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_2D : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5.0f;
+    [SerializeField] private float jumpPower = 2.0f;
+
+    private Rigidbody2D _playerRigidbody;
+    private void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        if (_playerRigidbody == null)
+        {
+            Debug.LogError("Player is missing a Rigidbody2D component");
+        }
+    }
+    private void Update()
+    {
+        MovePlayer();
+
+        if (Input.GetKey("space"))
+        {
+            Jump();
+        }
+    }
+    private void MovePlayer()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        _playerRigidbody.velocity = new Vector2(horizontalInput * playerSpeed, _playerRigidbody.velocity.y);
+    }
+
+    private void Jump() => _playerRigidbody.velocity = new Vector2(0, jumpPower);
+
+}
+
+
