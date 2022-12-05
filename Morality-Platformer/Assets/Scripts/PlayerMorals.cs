fbda55648c774a73da46eeacfadf51891139301e
DarrenMoye <darrenmoye02@gmail.com>
2022-12-05 20:18:46 +0000

    Continued 2d scene

--- Morality-Platformer/Assets/Scripts/PlayerMorals.cs
+++ Morality-Platformer/Assets/Scripts/PlayerMorals.cs
@@ -1,51 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMorals : MonoBehaviour
 {
     public int Morality = 0;
 
-    public GameObject Player;
-    public float Player_X;
-
-    int Rand;
-
-    public GameObject Selfish_Button;
-    public GameObject Selfless_Button;
+    public GameObject Button1;
+    public GameObject Button2;
     public GameObject Question1;
 
-    private int Update()
-    {
-        Player_X = Player.transform.position.x;
-        Rand = ((int)Player_X) % 2;
-        
-        return Rand;
-    }
+    public GameObject Button3;
+    public GameObject Button4;
+    public GameObject Question2;
+
+    public GameObject Button5;
+    public GameObject Button6;
+    public GameObject Question3;
 
     private void OnTriggerEnter(Collider Other)
     {
-        if (Other.gameObject.CompareTag("Selfish_Button") || Other.gameObject.CompareTag("Selfless_Button"))
+        if (Other.gameObject.CompareTag("Button1") || Other.gameObject.CompareTag("Button2"))
         {
-            Morality = SelfishvsSelfless(Other, Morality);
+            Morality = Question_One(Other, Morality);
             
-            Destroy(Selfish_Button);
-            Destroy(Selfless_Button);
+            Destroy(Button1);
+            Destroy(Button2);
             Destroy(Question1);
         }
-        
+        else if(Other.gameObject.CompareTag("Button3")|| Other.gameObject.CompareTag("Button4"))
+        {
+            Morality = Question_Two(Other, Morality);
 
+            Destroy(Button3);
+            Destroy(Button4);
+            Destroy(Question2);
+        }
+        else if(Other.gameObject.CompareTag("Button5")|| Other.gameObject.CompareTag("Button6"))
+        {
+            Morality = Question_Two(Other, Morality);
 
+            Destroy(Button5);
+            Destroy(Button6);
+            Destroy(Question3);
+        }
+        else
+        {
+            Transition();
+        }
     }
 
-    public int SelfishvsSelfless(Collider Other, int Moarlity)
+    public int Question_One(Collider Other, int Moarlity)
     {
-        if(Other.CompareTag("Selfish_Button"))
+        if(Other.CompareTag("Button1"))
         {
             Morality--;
             return Morality;
         }
-        else if(Other.CompareTag("Selfless_Button"))
+        else if(Other.CompareTag("Button2"))
         {
             Morality++;
             return Morality;
@@ -53,6 +66,63 @@ public class PlayerMorals : MonoBehaviour
         else
         {
             return Morality;
+        }
+    }
+
+    public int Question_Two(Collider Other, int Morality)
+    {
+        if (Other.CompareTag("Button3"))
+        {
+            Morality++;
+            return Morality;
+        }
+        else if (Other.CompareTag("Button4"))
+        {
+            Morality--;
+            return Morality;
+        }
+        else
+        {
+            return Morality;
+        }
+    }
+
+    public int Question_Three(Collider Other, int Morality)
+    {
+        if (Other.CompareTag("Button5"))
+        {
+            Morality++;
+            return Morality;
+        }
+        else if (Other.CompareTag("Button6"))
+        {
+            Morality--;
+            return Morality;
+        }
+        else
+        {
+            return Morality;
+        }
+    }
+
+    public void Transition()
+    {
+        if(Morality >= -3 && Morality <=-1)
+        {
+
+        }
+        else if(Morality > -1 && Morality < 1)
+        {
+
+        }
+        else if(Morality <= 1 && Morality <= 3)
+        {
+
+        }
+        else
+        {
+            SceneManager.LoadScene("");
+            SceneManager.UnloadScene("");
         }
     }
 }
